0fa99e6d712b8201a9ca9f3ac9fe29d115333d5b
Sunil Joshi <joshisunil2601@gmail.com>
2022-12-11 17:39:14 +0530

    productDetail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 // import { getProductdata } from "./api";
 import Button from "./Button";
 import Footer from "./Component/Footer/Footer";
 import NavBar from "./Component/NavBar/NavBar";
 import Product from "./Component/Product/Product";
 import ProductListPage from "./Component/ProductListPage/ProductListPage";
+import ProductDetail from "./ProductDetails";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="">
+    <div className=" h-screen">
       <NavBar />
-      <div>
-        <ProductListPage />
+      <div className="grow">
+        <Routes>
+          <Route index element={<ProductListPage />}></Route>
+
+          <Route path="/products/:id/" element={<ProductDetail />}></Route>
+        </Routes>
       </div>
       <Footer />
       {/* <Button>Save</Button> */}
--- src/Loading.tsx
+++ src/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ImSpinner6 } from "react-icons/im";
 function Loading() {
   return (
-    <div className="  text-black text-6xl flex justify-center mt-40 items-center">
+    <div className="  text-black text-6xl flex justify-center py-40 items-center">
       Loading...
       <ImSpinner6 className="animate-spin" />
     </div>
